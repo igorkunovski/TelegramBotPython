a5dea353d5179df3fec383d075d581e1023762cb
igorkunovski <igork110@mail.ru>
2022-11-24 10:22:42 +0200

    separated bot_commands and functions

--- commands.py
+++ commands.py
@@ -1,61 +1,33 @@
-import emoji
 import telebot
 import _token
-import calc_ration
-import calc_complex
-
+import functions
 
 bot = telebot.TeleBot(_token.API_TOKEN)
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, "Возможные команды:")
-    bot.send_message(message.chat.id, "/help - список мои команд")
-    bot.send_message(message.chat.id, "/calc - калькулятор простых чисел")
-    bot.send_message(message.chat.id, "/cplx - калькулятор комплексных чисел")
-    bot.send_message(message.chat.id, "/game - игра ;)")
+    functions.start_message(message)
 
 
 @bot.message_handler(commands=['help'])
-def start_message(message):
-    bot.send_message(message.chat.id, f'/help - список мои команд {emoji.emojize(":relaxed:")}')
-    bot.send_message(message.chat.id, "/calc - калькулятор простых чисел")
-    bot.send_message(message.chat.id, "/cplx - калькулятор комплексных чисел")
-    bot.send_message(message.chat.id, "/game - игра ;)")
+def help_message(message):
+    functions.help_message(message)
 
 
 @bot.message_handler(commands=['calc'])
 def calc_message(message):
-    eq_list = message.text.split("/calc")
-    if (len(eq_list) > 1) and eq_list[1] != "":
-        expr = str(eq_list[1])
-        result = calc_ration.simple_calc(expr)
-        bot.send_message(message.chat.id, f'Это элементарно! {expr} = {result}')
-    else:
-        bot.send_message(message.chat.id, "необходимо ввести выражение в формате /calc 1+1")
-        bot.send_message(message.chat.id, "Возможные символы для корректной работы + - / * ( )")
+    functions.calc_message(message)
 
 
 @bot.message_handler(commands=['cplx'])
 def calc_complex_message(message):
-    eq_list = message.text.split("/cplx")
-    if (len(eq_list) > 1) and eq_list[1] != "":
-        expr = str(eq_list[1])
-        result = str(calc_complex.calc_complex(expr))
-        bot.send_message(message.chat.id, f'Хмм.. {expr} = {result}')
-    else:
-        bot.send_message(message.chat.id, "необходимо ввести выражение в формате /cplx 1+1j")
-        bot.send_message(message.chat.id, f'Возможные символы для корректной работы + - * / ( ) j')
+    functions.calc_complex_message(message)
 
 
 @bot.message_handler(content_types='text')
 def message_reply(message):
-    if "привет".upper() in message.text.upper():
-        bot.send_message(message.chat.id, 'и тебе ПРИВЕТ')
-        bot.send_message(message.chat.id, "/help - список моих команд")
-    else:
-        bot.send_message(message.chat.id, "/help - список моих команд")
+    functions.message_reply(message)
 
 
 bot.polling()
--- functions.py
+++ functions.py
@@ -1,2 +1,49 @@
+import emoji
+import calc_complex
+import calc_ration
 import commands
 
+
+def start_message(message):
+    commands.bot.send_message(message.chat.id, "Возможные команды:")
+    commands.bot.send_message(message.chat.id, "/help - список мои команд")
+    commands.bot.send_message(message.chat.id, "/calc - калькулятор простых чисел")
+    commands.bot.send_message(message.chat.id, "/cplx - калькулятор комплексных чисел")
+    commands.bot.send_message(message.chat.id, "/game - игра ;)")
+
+
+def help_message(message):
+    commands.bot.send_message(message.chat.id, f'/help - список мои команд {emoji.emojize(":relaxed:")}')
+    commands.bot.send_message(message.chat.id, "/calc - калькулятор простых чисел")
+    commands.bot.send_message(message.chat.id, "/cplx - калькулятор комплексных чисел")
+    commands.bot.send_message(message.chat.id, "/game - игра ;)")
+
+
+def calc_message(message):
+    eq_list = message.text.split("/calc")
+    if (len(eq_list) > 1) and eq_list[1] != "":
+        expr = str(eq_list[1])
+        result = calc_ration.simple_calc(expr)
+        commands.bot.send_message(message.chat.id, f'Это элементарно! {expr} = {result}')
+    else:
+        commands.bot.send_message(message.chat.id, "необходимо ввести выражение в формате /calc 1+1")
+        commands.bot.send_message(message.chat.id, "Возможные символы для корректной работы + - / * ( )")
+
+
+def calc_complex_message(message):
+    eq_list = message.text.split("/cplx")
+    if (len(eq_list) > 1) and eq_list[1] != "":
+        expr = str(eq_list[1])
+        result = str(calc_complex.calc_complex(expr))
+        commands.bot.send_message(message.chat.id, f'Хмм.. {expr} = {result}')
+    else:
+        commands.bot.send_message(message.chat.id, "необходимо ввести выражение в формате /cplx 1+1j")
+        commands.bot.send_message(message.chat.id, f'Возможные символы для корректной работы + - * / ( ) j')
+
+
+def message_reply(message):
+    if "привет".upper() in message.text.upper():
+        commands.bot.send_message(message.chat.id, 'и тебе ПРИВЕТ')
+        commands.bot.send_message(message.chat.id, "/help - список моих команд")
+    else:
+        commands.bot.send_message(message.chat.id, "/help - список моих команд")
